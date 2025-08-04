1a5fb7c49c13f8f0770c3651c50110e5b488e903
vi-mar-aa <vitoria13102015@gmail.com>
2025-08-04 13:05:54 -0300

    Models, repositories e incio do login API

---
+++ API/LitteraAPI/Models/Mfuncionario.cs
@@ -0,0 +1,21 @@
+namespace LitteraAPI.Models;
+
+public class Mfuncionario
+{
+    
+}
+
+/*Classes
+ * cargo funcionario
+ *tipo midia
+ *midia
+ *denuncia
+ *emprestimo
+ *reserva
+ *mensagem
+ *lista de desejos
+ *cliente
+ *forum
+ *
+
+ */
---
+++ API/LitteraAPI/Routes/Rcliente.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using LitteraAPI.Repositories;
+using LitteraAPI.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LitteraAPI.Routes;
+
+public class Rcliente
+{
+    
+}
